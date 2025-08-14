59bd3c5cba690166fc0e2ceb4c7909780389af08
Jonathan Gomez <jonathangomez.contact@gmail.com>
2025-08-14 15:23:49 -0300

    Solución propagación del evento

--- 7.2/eventos.js
+++ 7.2/eventos.js
@@ -1,3 +1,8 @@
 document.getElementById('div').addEventListener("click", () => {
     alert('Hola! Soy el div')
 })
+
+document.getElementById('btn').addEventListener("click", (event) => {
+    event.stopPropagation(); // Evita que el click llegue al div
+    alert('Hola!');
+});
